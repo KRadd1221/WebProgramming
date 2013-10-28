f983a6f0cee507ec2d87b81008e93fcd8d51dbb0
unknown <Lori@.(none)>
2013-10-28 14:33:33 -0400

    10/28/13 preclass

--- Final/Models/Users.php
+++ Final/Models/Users.php
@@ -8,28 +8,41 @@ class Users {
         static public function Get($id=null)
         {
                 if(isset($id)){
-                        return fetch_one("SELECT * FROM Users WHERE id=$id");                        
+                        return fetch_one("SELECT * FROM 2013Fall_Users WHERE id=$id");                        
                 }else{
-                        return fetch_all('SELECT * FROM Users');                        
+                        return fetch_all('SELECT * FROM 2013Fall_Users');                        
                 }
         }
-        static public function Save($row)
-		{
-			$sql = 	"Insert Into Users (FirstName, LastName, Password, UserType)"
-				.	" Values ('$row[FirstName]', '$row[Lastname]', $row[Password]', '$row[UserType]') ";
-
-			$conn = GetConnection();
-			$conn -> query($sql);
-			echo $conn -> error;
-			$conn -> close();
-			
-			if($error){
-				return array('db_error' => $error);
-				
-			}else{
-				return false;
-			}
-							
-		}
         
+        static public function Blank()
+        {
+                return array( 'FirstName'=> null,'LastName'=> null,'Password'=> null,'UserType'=> null );
+        }
+        
+        static public function Save($row)
+        {
+                $sql =         " Insert Into 2013Fall_Users (FirstName, LastName, Password, UserType) "
+                        .        " Values ('$row[FirstName]', '$row[LastName]', '$row[Password]', '$row[UserType]') ";
+                $conn = GetConnection();
+                $conn->query($sql);
+                $error = $conn->error;                
+                $conn->close();
+                
+                if($error){
+                        return array('db_error' => $error);
+                }else {
+                        return false;
+                }
+        }
+        
+        static public function Validate($row)
+        {
+                $errors = array();
+                if(!$row['FirstName']) $errors['FirstName'] = 'id required';
+                if(!$row['LastName']) $errors['LastName'] = 'id required';
+                if(!is_numeric( $row['UserType'])) $errors['UserType'] = 'must be a number';
+                if(!$row['UserType']) $errors['UserType'] = 'id required';
+                
+                return count($errors) ? $errors : null;
+        }
 }
