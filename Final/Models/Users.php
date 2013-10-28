a4e4cad38e7e4c024e0f8d3063199bcbe87d14f9
unknown <Lori@.(none)>
2013-10-28 15:05:19 -0400

    vdeds

--- Final/Models/Users.php
+++ Final/Models/Users.php
@@ -8,9 +8,9 @@ class Users {
         static public function Get($id=null)
         {
                 if(isset($id)){
-                        return fetch_one("SELECT * FROM 2013Fall_Users WHERE id=$id");                        
+                        return fetch_one("SELECT * Users WHERE id=$id");                        
                 }else{
-                        return fetch_all('SELECT * FROM 2013Fall_Users');                        
+                        return fetch_all('SELECT * Users');                        
                 }
         }
         
