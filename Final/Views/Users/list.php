3c2ac68469d3bac73dee1f6144cd0355210646ea
unknown <n02704859@.active.newpaltz.edu>
2013-11-18 15:56:53 -0500

    11/18/13 class

--- Final/Views/Users/list.php
+++ Final/Views/Users/list.php
@@ -1,7 +1,11 @@
 <link href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" type="text/css" rel="stylesheet" />
 <style>
-        tr.success, tr.success td{
-                background-color: #00AA00;
+        .table tr.success2, .table tr.success2 td{
+                background-color: #FFAA00 !important; 
+        }
+        #table-wrapper{
+                transition: width .5s;
+                -webkit-transition: width .5s;
         }
 </style>
 <div class="container">
@@ -16,7 +20,7 @@
         <? endif; ?>
         
         <a href="?action=new">Add Contact</a>
-        
+        <div id="table-wrapper" class="col-md-12">
         <table class="table table-hover table-bordered table-striped">
                 <thead>
                 <tr>
@@ -28,32 +32,67 @@
                 </thead>
                 <tbody>
                 <? foreach ($model as $rs): ?>
-                        <tr class=" <?= $rs['id']==$_REQUEST['id'] ? 'success' : '' ?> ">
-                                <td><?=$rs['FirstName']?></td>
-                                <td><?=$rs['LastName']?></td>
-                                <td><?=$rs['UserType_Name']?></td>
-                                <td>
-                                        <a class="glyphicon glyphicon-file" href="?action=details&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="#myModal"></a>
-                                        <a class="glyphicon glyphicon-pencil" href="?action=edit&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="#myModal"></a>
-                                        <a class="glyphicon glyphicon-trash" href="?action=delete&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="#myModal"></a>
-                                </td>
-                        </tr>
+                        <? include 'item.php'; ?>
                 <? endforeach ?>
                 </tbody>
         </table>
+        </div>
+        <div id="details" class="col-md-6"></div>
 </div>
 
-<div id="myModal" class="modal fade"></div>
+<div id="myModal" class="modal fade">
+        
+</div>
+
 
 </div>
   <? function Scripts(){ ?>
         <script src="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
         <script type="text/javascript">
         $(function(){
                 $(".table").dataTable();
                 $(".alert .close").click(function(){
                         $(this).closest(".alert").slideUp();
                 });
+                
+                /*
+                $(".table tr").click(function(){
+                });
+                */
+                $(".table a").click(function(){
+                        
+                        
+                        if($(this).closest("tr").hasClass("success2")){
+                                $(".success2").removeClass("success2");
+                                $("#table-wrapper").removeClass("col-md-6").addClass("col-md-12");
+                                $("#details").html('');                        
+                        }else{
+                                $(".success2").removeClass("success2");
+                                $(this).closest("tr").addClass("success2");
+                                $("#table-wrapper").removeClass("col-md-12").addClass("col-md-6");
+                                
+                                $("#details").load(this.href, {format: "plain"}, function(){
+                                        $("#details form").submit(HandleSubmit);                                        
+                                });                                
+                        }
+                        
+                        return false;
+                });
+                
+                var HandleSubmit = function (){
+                        var data = $(this).serializeArray();
+                        data.push({name:'format', value:'plain'});
+                        $.post(this.action, data, function(results){
+                                if($(results).find("form").length){
+                                        $("#details").html(results);                                        
+                                }else{
+                                        $(".success2").html($(results).html())
+                                }
+                        });
+                        
+                        return false;
+                }
         })
         </script>
 <? } ?>
