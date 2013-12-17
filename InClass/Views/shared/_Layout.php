0a7fa1fa71fd11c4fc9cac1a2fa0cebab8b1c531
unknown <Kevin@.(none)>
2013-12-17 01:08:10 -0500

    done

--- InClass/Views/shared/_Layout.php
+++ InClass/Views/shared/_Layout.php
@@ -47,11 +47,11 @@
                 </div>
         </div>
 
-        <? include $view; ?>
+       
 
 
 
-
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script src="Scripts/main.js"></script>
